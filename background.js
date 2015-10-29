4a38d6d5eccd5dc26f552a505a68be70a0254dfe
Gilmore Davidson <gilmoreorless@gmail.com>
2015-10-29 12:45:03 +1100

    Got the caching working with multiple known CDNs
    Still has a GNDN button though, and is on by default.

--- background.js
+++ background.js
@@ -1,33 +1,58 @@
-var filter = {
-    urls: ['*://*.madcat/*', '*://ajax.googleapis.com/*'],
+var cdnDomains = [
+    'ajax.aspnetcdn.com',
+    'ajax.googleapis.com',
+    'cdn.jsdelivr.net',
+    'cdn.rawgit.com',
+    'cdnjs.cloudflare.com',
+    'code.jquery.com',
+    'aui-cdn.atlassian.com',
+    'api.bitbucket.org',
+];
+var filterDomains = cdnDomains.map(function (domain) {
+    return '*://' + domain + '/*';
+});
+var webRequestFilter = {
+    urls: filterDomains,
     types: ['stylesheet', 'script', 'image', 'object', 'xmlhttprequest']
 };
-var opt_extraInfoSpec = ['blocking'];
+var beforeRequestOptions = ['blocking'];
+
+/* Source: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22 */
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
 
 var beforeRequestCallback = function (details) {
     console.log('onBeforeRequest', details);
     if (details.method === 'GET' && details.tabId !== -1) {
-        if (details.type === 'stylesheet' && details.url.indexOf('github.madcat') > -1) {
-            return {
-                redirectUrl: chrome.extension.getURL('fake.css')
-            };
-        }
-        if (details.type === 'script' && details.url.indexOf('googleapis.com') > -1) {
-            var key = details.url;
-            var resBody = sessionStorage.getItem(key);
-            if (!resBody) {
+        var domain, mime;
+        for (var i = 0, ii = cdnDomains.length; i < ii; i++) {
+            domain = cdnDomains[i];
+            if (details.url.indexOf(domain) > -1) {
+                var key = details.url;
+                var resBody = sessionStorage.getItem(key);
+                console.log('Found item for key?', !!resBody, key);
+                if (resBody) {
+                    // TODO: Make this actually correct instead of guesswork
+                    mime = details.type === 'stylesheet' ? 'text/css' : 'application/javascript';
+                    return {
+                        redirectUrl: 'data:' + mime + ';base64,' + resBody
+                    };
+                }
                 fetch(key).then(function (response) {
                     return response.text();
                 }).then(function (body) {
-                    sessionStorage.setItem(key, btoa(body));
+                    try {
+                        sessionStorage.setItem(key, b64EncodeUnicode(body));
+                    } catch (e) {
+                        console.error('Failed to set item: ' + key, e);
+                    }
                 });
-            } else {
-                return {
-                    redirectUrl: 'data:application/javascript;base64,' + resBody
-                };
             }
         }
     }
 };
 
-chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, filter, opt_extraInfoSpec);
+chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
