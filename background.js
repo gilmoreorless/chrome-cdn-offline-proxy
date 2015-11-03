8b89dd4b71ae0bf3ee481e82e8decce798d47d61
Gilmore Davidson <gilmoreorless@gmail.com>
2015-11-04 08:33:35 +1100

    Toggle on/off the proxy with a browser action

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+/***** CONFIG *****/
+
 var cdnDomains = [
     'ajax.aspnetcdn.com',
     'ajax.googleapis.com',
@@ -6,8 +8,13 @@ var cdnDomains = [
     'cdnjs.cloudflare.com',
     'code.jquery.com',
     'aui-cdn.atlassian.com',
-    'api.bitbucket.org',
 ];
+
+/***** END CONFIG *****/
+
+
+///// Storage and options
+
 var filterDomains = cdnDomains.map(function (domain) {
     return '*://' + domain + '/*';
 });
@@ -16,6 +23,10 @@ var webRequestFilter = {
     types: ['stylesheet', 'script', 'image', 'object', 'xmlhttprequest']
 };
 var beforeRequestOptions = ['blocking'];
+var cacheIsActive = false;
+
+
+///// Helper methods
 
 /* Source: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22 */
 function b64EncodeUnicode(str) {
@@ -24,8 +35,10 @@ function b64EncodeUnicode(str) {
     }));
 }
 
-var beforeRequestCallback = function (details) {
-    console.log('onBeforeRequest', details);
+
+///// Caching and filter implemention
+
+function beforeRequestCallback(details) {
     if (details.method === 'GET' && details.tabId !== -1) {
         var domain, mime;
         for (var i = 0, ii = cdnDomains.length; i < ii; i++) {
@@ -33,7 +46,6 @@ var beforeRequestCallback = function (details) {
             if (details.url.indexOf(domain) > -1) {
                 var key = details.url;
                 var resBody = sessionStorage.getItem(key);
-                console.log('Found item for key?', !!resBody, key);
                 if (resBody) {
                     // TODO: Make this actually correct instead of guesswork
                     mime = details.type === 'stylesheet' ? 'text/css' : 'application/javascript';
@@ -53,6 +65,36 @@ var beforeRequestCallback = function (details) {
             }
         }
     }
-};
+}
 
-chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
+
+///// Browser action (enable/disable filter)
+
+function handleBrowserAction() {
+    setCacheActive(!cacheIsActive);
+}
+
+function setCacheActive(isActive) {
+    cacheIsActive = !!isActive;
+    if (cacheIsActive) {
+        chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
+        chrome.browserAction.setBadgeText({text: 'ON'});
+        chrome.browserAction.setIcon({
+            path: {
+                19: 'icons/icon-on-19.png',
+                38: 'icons/icon-on-38.png'
+            }
+        });
+    } else {
+        chrome.webRequest.onBeforeRequest.removeListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
+        chrome.browserAction.setBadgeText({text: ''});
+        chrome.browserAction.setIcon({
+            path: {
+                19: 'icons/icon-off-19.png',
+                38: 'icons/icon-off-38.png'
+            }
+        });
+    }
+}
+
+chrome.browserAction.onClicked.addListener(handleBrowserAction);
