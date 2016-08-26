0741b4ae1ad2a8911639e21a7fc3411eb2956d0c
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-08-26 17:44:29 +0200

    that should be more or less the implementation of chrome.storage.local (developed offline, of course)

--- background.js
+++ background.js
@@ -9,73 +9,102 @@ var cdnDomains = [
     'code.jquery.com',
     'aui-cdn.atlassian.com',
     'npmcdn.com'
+    // add your other CDN here!
 ];
 
 /***** END CONFIG *****/
 
-
-///// Storage and options
-
-var filterDomains = cdnDomains.map(function (domain) {
-    return '*://' + domain + '/*';
-});
 var webRequestFilter = {
-    urls: filterDomains,
+    urls: cdnDomains.map(function (domain) {
+        return '*://' + domain + '/*';
+    }),
     types: ['stylesheet', 'script', 'image', 'object', 'xmlhttprequest']
 };
-var beforeRequestOptions = ['blocking'];
-var cacheIsActive = false;
 
+// see https://developer.chrome.com/apps/offline_storage
+var storage = chrome.storage.local;
 
-///// Helper methods
+// internal copy of the chrome storage
+// TODO: investigate performance & memory consumption
+var cache = {}
 
-/* Source: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22 */
+/* see https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22 */
 function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
         return String.fromCharCode('0x' + p1);
     }));
 }
 
-// unfortionally `sessionStorage` has very strict quota of 5MB, see https://developer.chrome.com/apps/offline_storage 
-// unfortionally `chrome.storage.local` is async and does not work nicely together with the callback
-var storage = chrome.storage.local;
+function amountOfItemsInCache() {
+    return '' + Object.keys(cache).length;
+};
 
 ///// Caching and filter implemention
+function addListener() {
+
+    // load storage first, see: http://stackoverflow.com/a/18579032
+    storage.get(null, function (_cache) {
+        cache = _cache;
+        chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, webRequestFilter, ['blocking']);
+    });
+}
+
+function removeListener() {
+    chrome.webRequest.onBeforeRequest.removeListener(beforeRequestCallback);
+    cache = {};
+    storage.clear();
+}
+
+// returns content-type without charset ()
+function getContentType(headers, details) {
+    var contentTypeFallback = details.type === 'stylesheet' ? 'text/css' : 'application/javascript';
+    var contentType = headers.get('Content-Type') || contentTypeFallback;
+    return contentType
+        .replace(/charset=(.*)/i, '') // charset does not work together with base64
+        .replace(/;$/, '');
+}
 
 function beforeRequestCallback(details) {
 
     if (details.method === 'GET' && details.tabId !== -1) {
         for (var i = 0, ii = cdnDomains.length; i < ii; i++) {
-            
+
             var domain = cdnDomains[i];
             if (~details.url.indexOf(domain)) {
 
                 var key = details.url;
-                var cachedResponse = storage.get(key) // ?? TODO! -- see: http://stackoverflow.com/a/18579032
+                var cachedResponse = cache[key];
 
                 if (cachedResponse) {
-
-                    // TODO: Make this actually correct instead of guesswork
-                    var mime = details.type === 'stylesheet' ? 'text/css' : 'application/javascript';
-                    var resBody = cachedResponse.body;
-
                     return {
-                        redirectUrl: 'data:' + mime + ';base64,' + b64EncodeUnicode(resBody)
+                        redirectUrl: 'data:' + cachedResponse.contentType + ';base64,' + cachedResponse.body
                     };
                 }
 
-                fetch(key).then(function (resBody) {
-                    return resBody.text();
-                }).then(function (responseBody) {
+                fetch(key).then(function (response) {
+
+                    return response.text()
+                        .then(function (responseBody) {
+
+                            var body = b64EncodeUnicode(responseBody);
+                            var contentType = getContentType(response.headers, details);
+
+                            return {
+                                bodyEncoded: body,
+                                contentType: contentType
+                            };
+                        })
+                }).then(function (fullResponse) {
                     try {
 
-                        var resp = {
-                            key: {
-                                body: (responseBody)
-                            }
-                        }
+                        var storableResponse = {}
+                        storableResponse[key] = fullResponse
 
-                        storage.set(resp)
+                        // storing...
+                        storage.set(storableResponse);
+                        cache[key] = fullResponse;
+                        chrome.browserAction.setBadgeText({ text: amountOfItemsInCache() });
+
                     } catch (e) {
                         console.error('Failed to set item: ' + key, e);
                     }
@@ -87,6 +116,7 @@ function beforeRequestCallback(details) {
 
 
 ///// Browser action (enable/disable filter)
+var cacheIsActive = false;
 
 function handleBrowserAction() {
     setCacheActive(!cacheIsActive);
@@ -95,7 +125,7 @@ function handleBrowserAction() {
 function setCacheActive(isActive) {
     cacheIsActive = !!isActive;
     if (cacheIsActive) {
-        chrome.webRequest.onBeforeRequest.addListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
+        addListener();
         chrome.browserAction.setBadgeText({ text: 'ON' });
         chrome.browserAction.setIcon({
             path: {
@@ -104,7 +134,7 @@ function setCacheActive(isActive) {
             }
         });
     } else {
-        chrome.webRequest.onBeforeRequest.removeListener(beforeRequestCallback, webRequestFilter, beforeRequestOptions);
+        removeListener();
         chrome.browserAction.setBadgeText({ text: '' });
         chrome.browserAction.setIcon({
             path: {
